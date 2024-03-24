89392650f856320f58a66659b9f5f58fe9ce38e4
shota <shota19830706@gmail.com>
2024-03-24 16:04:14 +0900

    Adjust layout and styles for improved UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,22 +16,22 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="mt-32">
     <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-1/3 md:w-1/3 w-3/6 mb-10 md:mb-0">
                 <img class="object-cover object-center rounded" alt="hero" src="/images/top_image.png">
             </div>
             <div
                 class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                <h1 class="title-font sm:text-7xl text-3xl mb-4 font-light text-blue-900 tracking-widest">
                     CommuniCare
                 </h1>
-                <p class="leading-relaxed">CommuniCareは職員間の効率的なコミュニケーションと利用者様の情報を一元管理することで</p>
-                <br class="mb-8 leading-relaxed">介護施設運営の生産性を向上させることを目的としたプラットフォームです
-                <x-auth-links />
+                <p class="leading-relaxed">
+                    CommuniCareは職員間の効率的なコミュニケーションと利用者様の情報を一元管理することで介護施設運営の生産性を向上させることを目的としたプラットフォームです</p>
             </div>
         </div>
+        <x-auth-links />
     </section>
 </body>
 
