18d6dbc3493978fa03fc5ddc4e9cde1e175c2cce
shota <shota19830706@gmail.com>
2024-03-24 10:07:16 +0900

    Refactor navigation and button styles to make text Japanese

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,12 +29,7 @@
                 </h1>
                 <p class="leading-relaxed">CommuniCareは職員間の効率的なコミュニケーションと利用者様の情報を一元管理することで</p>
                 <br class="mb-8 leading-relaxed">介護施設運営の生産性を向上させることを目的としたプラットフォームです
-                <div class="flex justify-center">
-                    <button
-                        class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                    <button
-                        class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-                </div>
+                <x-auth-links />
             </div>
         </div>
     </section>
