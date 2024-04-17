5a1918f3faa05ffd23b461b6e560ef895a5a4a28
shota <shota19830706@gmail.com>
2024-04-17 19:18:06 +0900

    [update] ユーザー登録ルートのリファクタリングおよびビュー内のログイン/登録リンクのコメントアウト
    ユーザー登録のルーティングをゲストミドルウェアグループから外し、ルートの有効性に基づいてビュー内の認証リンクの表示を調整しました。

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,10 @@
             </div>
         </div>
     </div>
+    <div>
+        <a href="{{ route('register') }}"
+            class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+            新規登録
+        </a>
+    </div>
 </x-app-layout>
