ddb1956de48eba05c44896dc2de9418448d6ad0c
shota <shota19830706@gmail.com>
2024-04-25 10:28:02 +0900

    [update] ヘッダー部分のレイアウトとナビゲーションを復旧
    以前に採用していたヘッダー部分のレイアウトとナビゲーションを再度導入し、サイトの使いやすさとアクセシビリティを向上させました。

--- resources/js/components/DashboardComponent.vue
+++ resources/js/components/DashboardComponent.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <div class="font-semibold text-xl text-gray-800 leading-tight">
-            Dashboard
             <span v-if="user && user.role === 'admin'">- 管理者</span>
         </div>
 
@@ -22,12 +21,12 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="flex items-center pr-6 py-6 text-gray-900">
-                        <router-link
-                            to="/register"
+                        <a
+                            href="/register"
                             class="ml-4 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                         >
                             新規登録
-                        </router-link>
+                        </a>
                     </div>
                 </div>
                 <div
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,22 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div id="app">
-        <!-- Vue Router will handle all the pages rendering here -->
-        <dashboard-component></dashboard-component>
-        <example-component></example-component>
-    </div>
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <div id="app">
+            <!-- Vue Router will handle all the pages rendering here -->
+            <dashboard-component></dashboard-component>
+        </div>
 </body>
 
 </html>
