01e8a24f92ff110834bb5f00dccc7b37ef32127f
shota <shota19830706@gmail.com>
2024-05-21 04:11:55 +0900

    [add] users、comments、posts、residentsテーブルのマイグレーションファイル作成
    マルチテナンシー対応のテーブルに作り替えるためにdatabase/migrations/tenantにusers、comments、posts、residentsテーブルのマイグレーションファイルを作成します。この時点ではマイグレーションファイルはコミットされていないため、実行前の状態です。

---
+++ database/migrations/tenant/2024_05_20_185418_create_posts_table.php
@@ -0,0 +1,36 @@
+// database/migrations/tenant/yyyy_mm_dd_hhmmss_create_posts_table.php
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('message');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
