def6be083351c731923dcd0b2032742c796196b0
shota <shota19830706@gmail.com>
2024-06-13 11:17:53 +0900

    [fix] DOMContentLoadedイベント内の要素存在チェックを追加
    adminCheckboxおよびcontainerが存在しない場合にスクリプトがエラーを引き起こす可能性があったため、存在チェックを追加しました。

--- resources/js/persistAdminCheckboxState.js
+++ resources/js/persistAdminCheckboxState.js
@@ -1,23 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const adminCheckbox = document.getElementById("is_admin");
     const container = document.getElementById("nursing_home_container");
-    const storedIsAdmin = localStorage.getItem("isAdmin");
 
-    // ローカルストレージから状態を取得し、チェックボックスに適用
-    if (storedIsAdmin === "true") {
-        adminCheckbox.checked = true;
-        container.style.display = "block";
-        document.getElementById("tenant_name").required = true;
-    } else {
-        adminCheckbox.checked = false;
-        container.style.display = "none";
-        document.getElementById("tenant_name").required = false;
+    if (adminCheckbox && container) {
+        const storedIsAdmin = localStorage.getItem("isAdmin");
+
+        // ローカルストレージから状態を取得し、チェックボックスに適用
+        if (storedIsAdmin === "true") {
+            adminCheckbox.checked = true;
+            container.style.display = "block";
+            document.getElementById("tenant_name").required = true;
+        } else {
+            adminCheckbox.checked = false;
+            container.style.display = "none";
+            document.getElementById("tenant_name").required = false;
+        }
+
+        // チェックボックスの変更を監視し、ローカルストレージに保存
+        adminCheckbox.addEventListener("change", function () {
+            localStorage.setItem("isAdmin", adminCheckbox.checked);
+            container.style.display = adminCheckbox.checked ? "block" : "none";
+            document.getElementById("tenant_name").required =
+                adminCheckbox.checked;
+        });
     }
-
-    // チェックボックスの変更を監視し、ローカルストレージに保存
-    adminCheckbox.addEventListener("change", function () {
-        localStorage.setItem("isAdmin", adminCheckbox.checked);
-        container.style.display = adminCheckbox.checked ? "block" : "none";
-        document.getElementById("tenant_name").required = adminCheckbox.checked;
-    });
 });
