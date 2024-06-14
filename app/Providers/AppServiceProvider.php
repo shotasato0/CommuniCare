35007e1e7365fa751baee5b60dbb3d13b972cdb0
shota <shota19830706@gmail.com>
2024-06-14 11:03:49 +0900

    [fix] クエリリスナーの重複登録を防止
    クエリリスナーが重複して登録されないようにフラグを追加し、クエリ実行時のログを記録する機能を改善しました。これにより、不要なログの出力が抑制されます。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+namespace App\Providers;
+
 use Illuminate\Support\ServiceProvider;
 use App\Resolvers\CustomCachedTenantResolver;
 use Stancl\Tenancy\Resolvers\Contracts\CachedTenantResolver;
@@ -9,15 +11,24 @@ use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $booted = false; // クエリリスナー登録のフラグ
+
     public function register()
     {
         $this->app->singleton(CachedTenantResolver::class, CustomCachedTenantResolver::class);
     }
 
     public function boot()
-    {
+{
+    if (!$this->app->bound('query.listening')) {
         DB::listen(function ($query) {
             info("クエリを実行しています: {$query->sql} with バインディング: " . implode(', ', $query->bindings));
         });
+        $this->app->instance('query.listening', true);
+        info("クエリリスナーが登録されました");
     }
 }
+
+
+}
+
