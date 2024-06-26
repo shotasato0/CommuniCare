771e38177fc87805c2c9c8076669b9da5f2419ed
佐藤翔太 <shota19830706@gmail.com>
2024-06-26 22:14:12 +0900

    [update] ルートファイルのコメント追加
    ルートに適用するミドルウェアとそのコメントを追加し、コードの可読性を向上させました。

--- routes/tenant.php
+++ routes/tenant.php
@@ -1,29 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Support\Facades\Route;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-/*
-|--------------------------------------------------------------------------
-| Tenant Routes
-|--------------------------------------------------------------------------
-|
-| Here you can register the tenant routes for your application.
-| These routes are loaded by the TenantRouteServiceProvider.
-|
-| Feel free to customize them however you want. Good luck!
-|
-*/
-
+// ルートに適用するミドルウェアを指定します。 'web' ミドルウェアとStancl\Tenancyの二つのミドルウェアを適用します。
 Route::middleware([
-    'web',
-    InitializeTenancyByDomain::class,
-    PreventAccessFromCentralDomains::class,
-])->group(function () {
-    Route::get('/', function () {
+    'web', // Laravelの標準的なウェブアプリケーションのミドルウェアスタック（セッション、CSRF保護などを含む）
+    InitializeTenancyByDomain::class, // ドメインによってテナントを初期化するミドルウェア
+    PreventAccessFromCentralDomains::class, // 中央ドメインからのアクセスを防ぐミドルウェア
+])->group(function () { // ミドルウェアを適用したルートグループを定義します
+    // ルートグループ内のルートを定義します
+    Route::get('/', function () { // HTTP GETリクエストに応答するルートを定義します（ルートパスは '/'）
+        // テナントIDを含むメッセージを返します
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        // 'tenant' ヘルパー関数を使用して現在のテナントIDを取得し、それを返す文字列に追加します
     });
 });
