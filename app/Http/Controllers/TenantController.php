b65477e6bae4c153f77b8f9c954b6248793d30b1
佐藤翔太 <shota19830706@gmail.com>
2024-06-29 09:44:07 +0900

    [add]TenantControllerにテナント登録機能を追加
    テナントの登録フォーム表示と登録処理を行うメソッドをTenantControllerに追加しました。

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tenant;
 
 class TenantController extends Controller
 {
-    //
+    public function showRegistrationForm()
+    {
+        return inertia('TenantRegister');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'tenant_name' => 'required|string|max:255',
+        ]);
+
+        Tenant::create([
+            'name' => $request->tenant_name,
+        ]);
+
+        return redirect()->route('register'); // 新規登録ビューに遷移
+    }
 }
