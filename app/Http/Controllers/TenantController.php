4f147ba2576e7f61993b46b27c9b06823f696635
佐藤翔太 <shota19830706@gmail.com>
2024-07-02 05:23:26 +0900

    [update] デバッグ用ログ出力を削除してコードをクリーンアップ
    テナント登録後に新規登録画面に遷移する機能のデバッグが完了したため、デバッグ用のログ出力を削除してコードをクリーンアップしました。

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -18,13 +18,9 @@ class TenantController extends Controller
             'tenant_name' => 'required|string|max:255',
         ]);
 
-        \Log::info('Validated Tenant name:', ['tenant_name' => $request->tenant_name]);
-
         $tenant = Tenant::create([
             'name' => $request->tenant_name,
         ]);
-
-        \Log::info('Tenant created:', ['tenant' => $tenant]);
 
         session(['tenant_id' => $tenant->id]);
 
