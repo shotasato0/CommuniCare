d8f198dd658f79d52b7e3bc4aaa0a255e0c4528c
佐藤翔太 <shota19830706@gmail.com>
2024-07-09 18:44:52 +0900

    [remove] bootstrap/app.phpのCORSミドルウェア登録を削除
    CORSミドルウェアの削除に伴い、bootstrap/app.phpからCORSミドルウェアの登録を削除しました。これにより、設定がconfig/cors.phpに統一されます。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\InitializeTenancyCustom;
-use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(prepend: [
-            CorsMiddleware::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             InitializeTenancyCustom::class,
