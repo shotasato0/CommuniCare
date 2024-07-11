6024940ec9af632d89e275ca0ba75c578e6d3717
佐藤翔太 <shota19830706@gmail.com>
2024-07-11 11:33:23 +0900

    [update]Axiosの設定を元に戻しました
    Inertia.jsを使用してリクエストを送信するため、Axiosの設定は不要と判断し、以前の設定に戻しました。

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = 'http://localhost:9000';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
