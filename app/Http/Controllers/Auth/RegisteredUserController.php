dafc44fdc70681cf67e7ed836984b9f50239126a
佐藤翔太 <shota19830706@gmail.com>
2024-07-21 21:58:37 +0900

    [update]ユーザー登録時のバリデーションをusername_idに変更し、emailを削除
    ユーザー認証にusername_idを使用する方針に従い、バリデーションおよび保存処理を修正し、emailを削除しました。

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,13 +32,13 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'username_id' => 'required|string|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
-            'email' => $request->email,
+            'username_id' => $request->username_id,
             'password' => Hash::make($request->password),
         ]);
 
