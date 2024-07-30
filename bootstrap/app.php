bc4535b703d7b2d2ebcdfda8ac61c9982183dfc8
佐藤翔太 <shota19830706@gmail.com>
2024-07-30 17:53:27 +0900

    [update] SetTenantCookieミドルウェアの登録を追加
    SetTenantCookieミドルウェアを適用するため、useステートメントとミドルウェアリストにSetTenantCookie::classを追加しました。これにより、リクエスト時に動的にセッションのドメインが設定されるようになります。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\InitializeTenancyCustom;
+use App\Http\Middleware\SetTenantCookie;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             InitializeTenancyCustom::class,
+            SetTenantCookie::class,
         ]);
 
         //
