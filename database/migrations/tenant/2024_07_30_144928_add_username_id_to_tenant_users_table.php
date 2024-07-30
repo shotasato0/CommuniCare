3aa412f7c30f2015d0612cd96588f5df4e11e860
佐藤翔太 <shota19830706@gmail.com>
2024-07-30 15:04:53 +0900

    [update] usersテーブルに新しいカラムを追加（tenantディレクトリ）
    database/migrations/tenantディレクトリに配置したマイグレーションファイルに、usersテーブルのusername_id、unit_id、tenant_id、icon、telカラムを追加しました。このマイグレーションを実行することにより、すべてのテナントに対して新しいカラムが追加されます。

--- database/migrations/tenant/2024_07_30_144928_add_username_id_to_tenant_users_table.php
+++ database/migrations/tenant/2024_07_30_144928_add_username_id_to_tenant_users_table.php
@@ -8,21 +8,33 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up(): void
     {
-        Schema::table('tenant_users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username_id')->unique()->nullable()->after('id');
+            $table->unsignedBigInteger('unit_id')->nullable()->after('username_id');
+            $table->string('tenant_id')->after('unit_id');
+            $table->string('icon')->nullable()->after('tenant_id');
+            $table->string('tel')->nullable()->after('icon');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down(): void
     {
-        Schema::table('tenant_users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username_id');
+            $table->dropColumn('unit_id');
+            $table->dropColumn('tenant_id');
+            $table->dropColumn('icon');
+            $table->dropColumn('tel');
         });
     }
 };
