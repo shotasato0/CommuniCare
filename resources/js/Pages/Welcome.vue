d6538b1d8fe5659316a3b12580ccefb5c615803a
佐藤翔太 <shota19830706@gmail.com>
2024-08-07 15:55:32 +0900

    [update] TenantRegisterボタンのリンクテキストを仕様変更
    Registerボタンのリンクテキストを'TenantRegister'に変更し、テナント登録ページに遷移することを分かりやすくしました。

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -65,18 +65,11 @@ function handleImageError() {
 
                         <template v-else>
                             <Link
-                                :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
-                                Log in
-                            </Link>
-
-                            <Link
                                 v-if="canRegister"
                                 :href="route('tenant.register')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
-                                Register
+                                TenantRegister
                             </Link>
                         </template>
                     </nav>
