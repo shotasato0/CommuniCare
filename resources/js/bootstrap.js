18cb5523b6e6c30069fc2888b0d1ade57d718c41
佐藤翔太 <shota19830706@gmail.com>
2024-08-24 08:06:41 +0900

    [remove] CSRFトークンをリクエストヘッダーに自動設定する処理を削除
    不要なCSRFトークンのリクエストヘッダー自動設定処理を削除しました。これにより、コードの簡素化と重複の削減が図れます。
    CSRFトークンの設定は明示的に行わないと、419エラーが発生するため、この変更を行います。

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,6 +2,3 @@ import axios from "axios";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.headers.common["X-CSRF-TOKEN"] = document
-    .querySelector('meta[name="csrf-token"]')
-    .getAttribute("content");
