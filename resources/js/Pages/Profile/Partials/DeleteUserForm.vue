4e819329925cdac96cae641bb30ede938ecac81f
佐藤翔太 <shota19830706@gmail.com>
2024-09-03 11:06:29 +0900

    [update] アカウント削除セクションの日本語化対応
    Delete Account、Cancel、Password、およびアカウント削除に関連するテキストを vue-i18n を使用して日本語化できるように変更しました

--- resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -49,33 +49,41 @@ const closeModal = () => {
 <template>
     <section class="space-y-6">
         <header>
-            <h2 class="text-lg font-medium text-gray-900">Delete Account</h2>
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ $t("Delete Account") }}
+            </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                Once your account is deleted, all of its resources and data will
-                be permanently deleted. Before deleting your account, please
-                download any data or information that you wish to retain.
+                {{
+                    $t(
+                        "Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain."
+                    )
+                }}
             </p>
         </header>
 
-        <DangerButton @click="confirmUserDeletion">Delete Account</DangerButton>
+        <DangerButton @click="confirmUserDeletion">
+            {{ $t("Delete Account") }}
+        </DangerButton>
 
         <Modal :show="confirmingUserDeletion" @close="closeModal">
             <div class="p-6">
                 <h2 class="text-lg font-medium text-gray-900">
-                    Are you sure you want to delete your account?
+                    {{ $t("Are you sure you want to delete your account?") }}
                 </h2>
 
                 <p class="mt-1 text-sm text-gray-600">
-                    Once your account is deleted, all of its resources and data
-                    will be permanently deleted. Please enter your password to
-                    confirm you would like to permanently delete your account.
+                    {{
+                        $t(
+                            "Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account."
+                        )
+                    }}
                 </p>
 
                 <div class="mt-6">
                     <InputLabel
                         for="password"
-                        value="Password"
+                        :value="$t('Password')"
                         class="sr-only"
                     />
 
@@ -85,7 +93,7 @@ const closeModal = () => {
                         v-model="form.password"
                         type="password"
                         class="mt-1 block w-3/4"
-                        placeholder="Password"
+                        :placeholder="$t('Password')"
                         @keyup.enter="deleteUser"
                     />
 
@@ -94,7 +102,7 @@ const closeModal = () => {
 
                 <div class="mt-6 flex justify-end">
                     <SecondaryButton @click="closeModal">
-                        Cancel
+                        {{ $t("Cancel") }}
                     </SecondaryButton>
 
                     <DangerButton
@@ -103,7 +111,7 @@ const closeModal = () => {
                         :disabled="form.processing"
                         @click="deleteUser"
                     >
-                        Delete Account
+                        {{ $t("Delete Account") }}
                     </DangerButton>
                 </div>
             </div>
