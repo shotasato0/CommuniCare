6bd41f56f8e88328bbf9d63148973e67ebf6658b
佐藤翔太 <shota19830706@gmail.com>
2024-09-03 10:31:44 +0900

    [update] ダッシュボードページの日本語化対応と重複するユーザー名の表示を削除
    Dashboard の見出しおよび You're logged in のメッセージを vue-i18n を使用して日本語化しました。また、ユーザー名がヘッダーにも表示されているため、重複していたユーザー名の表示を削除しました。

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,11 +1,11 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, usePage } from '@inertiajs/vue3';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, usePage } from "@inertiajs/vue3";
 
 const { props } = usePage();
 
 // コンソールにユーザー情報を出力
-console.log('User data:', props.auth.user);
+console.log("User data:", props.auth.user);
 </script>
 
 <template>
@@ -13,14 +13,16 @@ console.log('User data:', props.auth.user);
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ $t('Dashboard') }}
+            </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        You're logged in, {{ props.auth.user.name }}
+                        {{ $t('You\'re logged in') }}
                     </div>
                 </div>
             </div>
