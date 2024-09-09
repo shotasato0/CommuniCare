ba193f27e2b05d87c56b5997d9df8ed78d61e374
佐藤翔太 <shota19830706@gmail.com>
2024-09-10 08:58:39 +0900

    [add] Postモデルの作成および投稿機能のリレーション追加
    掲示板の投稿機能を管理するためにPostモデルを作成し、Userモデルとのリレーションを追加しました。また、コメントや「いいね」機能に対応するリレーションも含め、投稿に対する操作ができるようにしました。

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['user_id', 'title', 'message', 'like_count'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable'); // ポリモーフィックリレーション
+    }
+}
+
