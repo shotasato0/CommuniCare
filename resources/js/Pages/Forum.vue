b337aa4bfdb9533093081970ffa1a2ccdab057a3
佐藤翔太 <shota19830706@gmail.com>
2024-09-10 14:57:22 +0900

    [refactor] AuthenticatedLayoutとのレイアウト統合とForum.vueのソースコード整理
    AuthenticatedLayoutをForum.vueの親コンポーネントとして統合し、レイアウトの一貫性を確保しました。また、他のコンポーネントの構成に合わせてソースコードの整理を行いました。

--- resources/js/Pages/Forum.vue
+++ resources/js/Pages/Forum.vue
@@ -1,119 +1,6 @@
-<template>
-    <div class="w-11/12 max-w-screen-md m-auto">
-        <h1 class="text-xl font-bold mt-5">{{ appName }}</h1>
-
-        <!-- 投稿フォーム -->
-        <div class="bg-white rounded-md mt-5 p-3">
-            <form @submit.prevent="submitPost">
-                <div class="flex">
-                    <p class="font-bold">名前</p>
-                    <input
-                        v-model="postData.user_name"
-                        class="border rounded px-2 ml-2"
-                        type="text"
-                        required
-                    />
-                </div>
-                <div class="flex mt-2">
-                    <p class="font-bold">件名</p>
-                    <input
-                        v-model="postData.message_title"
-                        class="border rounded px-2 ml-2 flex-auto"
-                        type="text"
-                        required
-                    />
-                </div>
-                <div class="flex flex-col mt-2">
-                    <p class="font-bold">本文</p>
-                    <textarea
-                        v-model="postData.message"
-                        class="border rounded px-2"
-                        required
-                    ></textarea>
-                </div>
-                <div class="flex justify-end mt-2">
-                    <button
-                        class="my-2 px-2 py-1 rounded bg-blue-300 text-blue-900 font-bold link-hover cursor-pointer"
-                    >
-                        投稿
-                    </button>
-                </div>
-            </form>
-        </div>
-
-        <!-- 検索フォーム -->
-        <div class="bg-white rounded-md mt-3 p-3">
-            <form @submit.prevent="searchPosts">
-                <div class="mx-1 flex">
-                    <input
-                        v-model="searchQuery"
-                        class="border rounded px-2 flex-auto"
-                        type="text"
-                        placeholder="検索"
-                    />
-                    <button
-                        class="ml-2 px-2 py-1 rounded bg-gray-500 text-white font-bold link-hover cursor-pointer"
-                    >
-                        検索
-                    </button>
-                </div>
-            </form>
-        </div>
-
-        <!-- 投稿一覧 -->
-        <div
-            v-for="(post, index) in posts"
-            :key="index"
-            class="bg-white rounded-md mt-1 mb-5 p-3"
-        >
-            <!-- スレッド -->
-            <div>
-                <p class="mb-2 text-xs">
-                    {{ post.created_at }} ＠{{ post.user_name }}
-                </p>
-                <p class="mb-2 text-xl font-bold">{{ post.message_title }}</p>
-                <p class="mb-2">{{ post.message }}</p>
-            </div>
-            <!-- 削除ボタン -->
-            <form
-                class="flex justify-end mt-5"
-                @submit.prevent="deletePost(post.id)"
-            >
-                <input
-                    v-model="replyMessage"
-                    class="border rounded px-2 flex-auto"
-                    type="text"
-                    placeholder="返信メッセージ"
-                />
-                <button
-                    class="px-2 py-1 ml-2 rounded bg-green-600 text-white font-bold link-hover cursor-pointer"
-                >
-                    返信
-                </button>
-                <button
-                    class="px-2 py-1 ml-2 rounded bg-red-500 text-white font-bold link-hover cursor-pointer"
-                >
-                    削除
-                </button>
-            </form>
-            <!-- 返信 -->
-            <hr class="mt-2 m-auto" />
-            <div class="flex justify-end">
-                <div class="w-11/12">
-                    <div v-for="(reply, index) in post.replies" :key="index">
-                        <p class="mt-2 text-xs">
-                            {{ reply.created_at }} ＠{{ reply.user_name }}
-                        </p>
-                        <p class="my-2 text-sm">{{ reply.message }}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script setup>
 import { ref } from "vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 // アプリ名とフォームデータ
 const appName = "CommuniCare";
@@ -156,3 +43,124 @@ const deletePost = (postId) => {
     opacity: 70%;
 }
 </style>
+
+<template>
+    <AuthenticatedLayout>
+        <div class="w-11/12 max-w-screen-md m-auto">
+            <h1 class="text-xl font-bold mt-5">{{ appName }}</h1>
+
+            <!-- 投稿フォーム -->
+            <div class="bg-white rounded-md mt-5 p-3">
+                <form @submit.prevent="submitPost">
+                    <div class="flex">
+                        <p class="font-bold">名前</p>
+                        <input
+                            v-model="postData.user_name"
+                            class="border rounded px-2 ml-2"
+                            type="text"
+                            required
+                        />
+                    </div>
+                    <div class="flex mt-2">
+                        <p class="font-bold">件名</p>
+                        <input
+                            v-model="postData.message_title"
+                            class="border rounded px-2 ml-2 flex-auto"
+                            type="text"
+                            required
+                        />
+                    </div>
+                    <div class="flex flex-col mt-2">
+                        <p class="font-bold">本文</p>
+                        <textarea
+                            v-model="postData.message"
+                            class="border rounded px-2"
+                            required
+                        ></textarea>
+                    </div>
+                    <div class="flex justify-end mt-2">
+                        <button
+                            class="my-2 px-2 py-1 rounded bg-blue-300 text-blue-900 font-bold link-hover cursor-pointer"
+                        >
+                            投稿
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <!-- 検索フォーム -->
+            <div class="bg-white rounded-md mt-3 p-3">
+                <form @submit.prevent="searchPosts">
+                    <div class="mx-1 flex">
+                        <input
+                            v-model="searchQuery"
+                            class="border rounded px-2 flex-auto"
+                            type="text"
+                            placeholder="検索"
+                        />
+                        <button
+                            class="ml-2 px-2 py-1 rounded bg-gray-500 text-white font-bold link-hover cursor-pointer"
+                        >
+                            検索
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <!-- 投稿一覧 -->
+            <div
+                v-for="(post, index) in posts"
+                :key="index"
+                class="bg-white rounded-md mt-1 mb-5 p-3"
+            >
+                <!-- スレッド -->
+                <div>
+                    <p class="mb-2 text-xs">
+                        {{ post.created_at }} ＠{{ post.user_name }}
+                    </p>
+                    <p class="mb-2 text-xl font-bold">
+                        {{ post.message_title }}
+                    </p>
+                    <p class="mb-2">{{ post.message }}</p>
+                </div>
+                <!-- 削除ボタン -->
+                <form
+                    class="flex justify-end mt-5"
+                    @submit.prevent="deletePost(post.id)"
+                >
+                    <input
+                        v-model="replyMessage"
+                        class="border rounded px-2 flex-auto"
+                        type="text"
+                        placeholder="返信メッセージ"
+                    />
+                    <button
+                        class="px-2 py-1 ml-2 rounded bg-green-600 text-white font-bold link-hover cursor-pointer"
+                    >
+                        返信
+                    </button>
+                    <button
+                        class="px-2 py-1 ml-2 rounded bg-red-500 text-white font-bold link-hover cursor-pointer"
+                    >
+                        削除
+                    </button>
+                </form>
+                <!-- 返信 -->
+                <hr class="mt-2 m-auto" />
+                <div class="flex justify-end">
+                    <div class="w-11/12">
+                        <div
+                            v-for="(reply, index) in post.replies"
+                            :key="index"
+                        >
+                            <p class="mt-2 text-xs">
+                                {{ reply.created_at }} ＠{{ reply.user_name }}
+                            </p>
+                            <p class="my-2 text-sm">{{ reply.message }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
