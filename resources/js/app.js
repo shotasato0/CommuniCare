d1b59111702ba972cc8d738864c6bdc501951420
佐藤翔太 <shota19830706@gmail.com>
2024-09-21 11:35:43 +0900

    [update] 投稿・返信・削除ボタンをBootstrap Iconsに変更
    app.jsにBootstrap IconsのCSSをインポートし、投稿、返信、削除ボタンをアイコンに置き換えてUIを変更しました。

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import "../css/app.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
