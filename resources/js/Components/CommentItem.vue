9ad55d4ea5dc06c289f6a54616c202761e3e049e
佐藤翔太 <shota19830706@gmail.com>
2024-09-25 09:38:00 +0900

    [remove] 返信ボタン機能を削除
    メンションを使った返信ロジックを導入するため、不要になった返信ボタン機能を削除しました。

--- resources/js/Components/CommentItem.vue
+++ resources/js/Components/CommentItem.vue
@@ -1,34 +1,21 @@
 <script setup>
 const props = defineProps({
     comment: Object, // 親コンポーネントから渡される単一のコメントデータ（オブジェクト型）
-    postId: Number, // 親コンポーネントから渡される投稿ID（数値型）。コメントが属する投稿を特定するために使用
+    postId: Number, // 投稿のIDを受け取る
     formatDate: Function, // 日付をフォーマットする関数を親から受け取る
-    isCommentAuthor: Function, // コメントの作成者かどうかを判定する関数。親コンポーネントから渡される
-    deleteItem: Function, // コメントを削除する関数。親コンポーネントから渡される
-    toggleCommentForm: Function, // コメントフォームの表示/非表示を切り替える関数。親コンポーネントから渡される
+    isCommentAuthor: Function, // コメントの作者かどうかを確認する関数
+    deleteItem: Function, // コメント削除の関数を親から受け取る
 });
 </script>
 
 <template>
     <div class="ml-4 mb-2">
         <p class="text-xs">
-            {{ formatDate(comment.created_at) }} ＠{{ comment.user?.name || "Unknown" }}
+            {{ formatDate(comment.created_at) }} ＠{{
+                comment.user?.name || "Unknown"
+            }}
         </p>
         <p>{{ comment.message }}</p>
-
-        <!-- 返信ボタン -->
-        <button
-            @click="
-                toggleCommentForm(
-                    postId,
-                    comment.id,
-                    comment.user?.name || 'Unknown'
-                )
-            "
-            class="px-2 py-1 rounded bg-green-500 text-white font-bold link-hover cursor-pointer"
-        >
-            <i class="bi bi-reply"></i>
-        </button>
 
         <!-- コメント削除ボタン -->
         <button
