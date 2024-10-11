dec0d72a9eef3910bd9c00ae33a8619f90c3b6a0
佐藤翔太 <shota19830706@gmail.com>
2024-10-12 08:01:17 +0900

    [add]ユーザー詳細ページの作成
    ユーザーの情報を表示するための詳細ページを作成しました。これにより、ユーザーのプロフィール情報を確認できるようになりました。

---
+++ resources/js/Pages/Users/Show.vue
@@ -0,0 +1,73 @@
+<script>
+import { router, usePage } from "@inertiajs/vue3";
+
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true,
+        },
+    },
+    setup() {
+        const page = usePage();
+        return { page };
+    },
+    methods: {
+        editUser() {
+            // 編集ボタンを押したときの処理をここに書く
+            router.visit(`/users/${this.user.id}/edit`);
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="flex justify-center items-center min-h-screen bg-gray-100">
+        <div class="w-80 bg-white p-6 rounded-lg shadow-lg text-center">
+            <img
+                :src="user.icon || 'https://via.placeholder.com/100'"
+                alt="ユーザーのプロフィール写真"
+                class="w-24 h-24 rounded-full object-cover mx-auto mb-4"
+            />
+            <button
+                @click="editUser"
+                class="bg-blue-400 text-white px-4 py-2 rounded mb-6 hover:bg-blue-500"
+            >
+                編集
+            </button>
+
+            <div class="info mb-4 text-left">
+                <label class="font-bold">名前</label>
+                <div class="border border-gray-300 p-2 rounded bg-gray-100">
+                    {{ user.name }}
+                </div>
+            </div>
+            <div class="info mb-4 text-left">
+                <label class="font-bold">電話番号</label>
+                <div class="border border-gray-300 p-2 rounded bg-gray-100">
+                    {{ user.tel }}
+                </div>
+            </div>
+            <div class="info mb-4 text-left">
+                <label class="font-bold">メールアドレス</label>
+                <div class="border border-gray-300 p-2 rounded bg-gray-100">
+                    {{ user.email }}
+                </div>
+            </div>
+            <div class="info mb-4 text-left">
+                <label class="font-bold">担当入居者</label>
+                <div class="border border-gray-300 p-2 rounded bg-gray-100">
+                    {{ user.unit_name }}
+                </div>
+            </div>
+            <div class="info mb-4 text-left">
+                <label class="font-bold">メモ</label>
+                <div class="border border-gray-300 p-2 rounded bg-gray-100">
+                    {{ user.memo }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
