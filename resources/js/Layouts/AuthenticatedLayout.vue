f00831e07ea26ae02af81fc8642a2772f2bc2375
佐藤翔太 <shota19830706@gmail.com>
2024-10-22 06:53:28 +0900

    [update] 検索フォームをユーザー名の左隣に配置するように変更
    検索フォームをナビゲーションバー内でユーザー名の左隣に配置するように変更し、ユーザードロップダウンメニューとのレイアウト調整を行いました。

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -49,67 +49,71 @@ const auth = usePage().props.auth || {};
                             </div>
                         </div>
 
-                        <!-- 検索フォーム -->
-                        <SearchForm />
+                        <div class="flex items-center space-x-4">
+                            <!-- 検索フォーム -->
+                            <SearchForm />
 
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                                <!-- Settings Dropdown -->
+                                <div class="ms-3 relative">
+                                    <Dropdown align="right" width="48">
+                                        <template #trigger>
+                                            <span
+                                                class="inline-flex rounded-md"
                                             >
-                                                <!-- user.nameがnullでないことを確認 -->
-                                                <span v-if="auth.user">
-                                                    {{ auth.user.name }}
-                                                </span>
-                                                <span v-else> ゲスト </span>
-
-                                                <svg
-                                                    class="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
+                                                <button
+                                                    type="button"
+                                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                                 >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
+                                                    <!-- user.nameがnullでないことを確認 -->
+                                                    <span v-if="auth.user">
+                                                        {{ auth.user.name }}
+                                                    </span>
+                                                    <span v-else> ゲスト </span>
 
-                                    <template #content>
-                                        <DropdownLink
-                                            :href="route('profile.edit')"
-                                            class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
-                                        >
-                                            {{ $t("Profile") }}
-                                        </DropdownLink>
-                                        <form
-                                            :action="route('logout')"
-                                            method="post"
-                                            class="inline"
-                                        >
-                                            <input
-                                                type="hidden"
-                                                name="_token"
-                                                :value="csrfToken"
-                                            />
-                                            <button
-                                                type="submit"
-                                                class="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                                                    <svg
+                                                        class="ms-2 -me-0.5 h-4 w-4"
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 20 20"
+                                                        fill="currentColor"
+                                                    >
+                                                        <path
+                                                            fill-rule="evenodd"
+                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </span>
+                                        </template>
+
+                                        <template #content>
+                                            <DropdownLink
+                                                :href="route('profile.edit')"
+                                                class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                                             >
-                                                {{ $t("Log Out") }}
-                                            </button>
-                                        </form>
-                                    </template>
-                                </Dropdown>
+                                                {{ $t("Profile") }}
+                                            </DropdownLink>
+                                            <form
+                                                :action="route('logout')"
+                                                method="post"
+                                                class="inline"
+                                            >
+                                                <input
+                                                    type="hidden"
+                                                    name="_token"
+                                                    :value="csrfToken"
+                                                />
+                                                <button
+                                                    type="submit"
+                                                    class="block w-full text-left px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                                                >
+                                                    {{ $t("Log Out") }}
+                                                </button>
+                                            </form>
+                                        </template>
+                                    </Dropdown>
+                                </div>
                             </div>
                         </div>
 
