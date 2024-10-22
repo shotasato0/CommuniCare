d5f4489b2ea468ca54996d82711b4eb9c98b3818
佐藤翔太 <shota19830706@gmail.com>
2024-10-23 08:13:06 +0900

    [update] 検索フォームのレイアウトを他のテキストボックスと統一
    検索フォームのデザインを他のテキストボックスと統一するため、rounded-mdをroundedに変更しました。

--- resources/js/Components/SearchForm.vue
+++ resources/js/Components/SearchForm.vue
@@ -46,7 +46,7 @@ const compositionEnd = () => {
             v-model="search"
             type="text"
             placeholder="投稿を検索"
-            class="border p-2 w-full pr-12 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500"
+            class="border p-2 w-full pr-12 rounded shadow-sm focus:ring-2 focus:ring-blue-500"
             @keydown.enter="searchPosts"
             @compositionstart="compositionStart"
             @compositionend="compositionEnd"
