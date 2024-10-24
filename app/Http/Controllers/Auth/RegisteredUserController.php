f8185177835793ce4f097630876251cfe6743562
佐藤翔太 <shota19830706@gmail.com>
2024-10-24 17:09:04 +0900

    [update] ユーザー登録後の自動ログイン処理を除外
    ユーザー登録後も既存のログインユーザーのまま操作を続けられるようにするため、自動ログイン処理を除外しました。

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,8 +46,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         return redirect()->route('dashboard')->with(['redirect' => true]);
     }
 }
