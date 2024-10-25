9a7b6a5e7e794e024762bdb3d95dc22ec8456cce
佐藤翔太 <shota19830706@gmail.com>
2024-10-25 21:10:04 +0900

    [update] 社員一覧ページのレイアウトを認証レイアウトに統合
    社員一覧ページを他のページと同様の認証レイアウト (AuthenticatedLayout) で表示することで、統一感のあるデザインを実現しました。

--- resources/js/Pages/Admin/EmployeeList.vue
+++ resources/js/Pages/Admin/EmployeeList.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { Head, usePage} from "@inertiajs/vue3";
+import { Head, usePage } from "@inertiajs/vue3";
 import { ref } from "vue";
 import Show from "@/Pages/Users/Show.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 const { props } = usePage();
 const users = props.users;
@@ -22,46 +23,48 @@ const closeUserProfile = () => {
 <template>
     <Head :title="$t('Employee List')" />
 
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-        <h1 class="text-2xl font-bold mb-6">{{ $t("Employee List") }}</h1>
+    <AuthenticatedLayout>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <h1 class="text-2xl font-bold mb-6">{{ $t("Employee List") }}</h1>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div
-                v-for="user in users"
-                :key="user.id"
-                class="bg-white overflow-hidden shadow rounded-lg p-4 flex items-center space-x-4"
-            >
-                <img
-                    :src="
-                        user.icon
-                            ? `/storage/${user.icon}`
-                            : 'https://via.placeholder.com/150'
-                    "
-                    alt="Profile Icon"
-                    class="w-16 h-16 rounded-full cursor-pointer hover:opacity-70"
-                    @click="openUserProfile(user)"
-                />
-
-                <p class="text-lg font-bold text-gray-900">
-                    <span
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div
+                    v-for="user in users"
+                    :key="user.id"
+                    class="bg-white overflow-hidden shadow rounded-lg p-4 flex items-center space-x-4"
+                >
+                    <img
+                        :src="
+                            user.icon
+                                ? `/storage/${user.icon}`
+                                : 'https://via.placeholder.com/150'
+                        "
+                        alt="Profile Icon"
+                        class="w-16 h-16 rounded-full cursor-pointer hover:opacity-70"
                         @click="openUserProfile(user)"
-                        class="hover:bg-blue-300 p-1 rounded cursor-pointer"
-                    >
-                        {{ user.name }}
-                    </span>
-                </p>
+                    />
+
+                    <p class="text-lg font-bold text-gray-900">
+                        <span
+                            @click="openUserProfile(user)"
+                            class="hover:bg-blue-300 p-1 rounded cursor-pointer"
+                        >
+                            {{ user.name }}
+                        </span>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- 選択された投稿のユーザーの詳細ページを表示 -->
-    <div
-        v-if="isUserProfileVisible"
-        class="fixed inset-0 bg-black/50 flex justify-center items-center z-50"
-        @click="closeUserProfile"
-    >
-        <div @click.stop>
-            <Show v-if="selectedUser" :user="selectedUser" />
+        <!-- 選択された投稿のユーザーの詳細ページを表示 -->
+        <div
+            v-if="isUserProfileVisible"
+            class="fixed inset-0 bg-black/50 flex justify-center items-center z-50"
+            @click="closeUserProfile"
+        >
+            <div @click.stop>
+                <Show v-if="selectedUser" :user="selectedUser" />
+            </div>
         </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
