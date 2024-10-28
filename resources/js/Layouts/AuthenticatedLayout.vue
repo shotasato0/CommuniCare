ee6b4b4f6c140622aa08aa79b10b722e65d242f8
佐藤翔太 <shota19830706@gmail.com>
2024-10-29 05:36:14 +0900

    [update] ナビゲーションバーの固定表示とメインコンテンツの位置調整
    ナビゲーションバーを固定表示し、メインコンテンツが隠れないように `mt-16` を追加しました。これにより、スクロール時もナビゲーションバーが画面上部に固定され、アプリ名や検索バーが常に表示されるようになりました。

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,7 +21,9 @@ const auth = usePage().props.auth || {};
 <template>
     <div>
         <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
+            <nav
+                class="bg-white border-b border-gray-100 fixed top-0 left-0 w-full z-10"
+            >
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
