d8a971fab0488b0fadc3d533865f482e4b504782
佐藤翔太 <shota19830706@gmail.com>
2024-10-29 07:42:27 +0900

    [update] サイドバーのマージン調整に伴うEmployeeListビューのレイアウト変更
    モバイル表示時には `mt-0`、デスクトップ表示時には `mt-16` にサイドバーのマージンを動的に設定する変更が、EmployeeListビューにも影響を与えたため、EmployeeListのタイトルに `mt-16` を追加して全体のレイアウトを調整しました。

--- resources/js/Pages/Admin/EmployeeList.vue
+++ resources/js/Pages/Admin/EmployeeList.vue
@@ -26,7 +26,7 @@ const closeUserProfile = () => {
 
     <AuthenticatedLayout>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            <h1 class="text-2xl font-bold mb-6">{{ $t("Employee List") }}</h1>
+            <h1 class="text-2xl font-bold mb-6 mt-16">{{ $t("Employee List") }}</h1>
 
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div
