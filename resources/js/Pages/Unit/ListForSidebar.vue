8871a9c6532c6e005bca3e6244071041a1591638
佐藤翔太 <shota19830706@gmail.com>
2024-10-31 09:56:31 +0900

    [update] 部署名のクリックで所属ユーザーを表示するプルダウン機能を追加
    部署名の横にプルダウンマークを追加し、クリック時に所属ユーザーを表示できるようにサイドバーコンポーネントを改善しました。

--- resources/js/Pages/Unit/ListForSidebar.vue
+++ resources/js/Pages/Unit/ListForSidebar.vue
@@ -1,34 +1,81 @@
+<script>
+export default {
+    props: {
+        units: {
+            type: Array,
+            required: true,
+        },
+        users: {
+            type: Array,
+            required: false,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            selectedUnitId: null,
+        };
+    },
+    computed: {
+        filteredUsers() {
+            if (this.selectedUnitId) {
+                return this.users.filter(
+                    (user) => user.unit_id === this.selectedUnitId
+                );
+            }
+            return [];
+        },
+    },
+    methods: {
+        toggleUnit(unitId) {
+            this.selectedUnitId =
+                this.selectedUnitId === unitId ? null : unitId;
+        },
+    },
+};
+</script>
+
 <template>
     <div class="sidebar bg-gray-100 w-60 h-screen p-4 shadow-lg">
-      <h2 class="text-xl font-bold mb-4">部署一覧</h2>
-      <ul>
-        <li 
-          v-for="unit in units" 
-          :key="unit.id" 
-          class="mb-2 p-2 rounded hover:bg-gray-200 cursor-pointer">
-          {{ unit.name }}
-        </li>
-      </ul>
+        <h2 class="text-xl font-bold mb-4">部署一覧</h2>
+        <ul>
+            <li
+                v-for="unit in units"
+                :key="unit.id"
+                class="mb-2 p-2 rounded hover:bg-gray-200 cursor-pointer"
+                @click="toggleUnit(unit.id)"
+            >
+                <div class="flex items-center justify-between">
+                    <span>{{ unit.name }}</span>
+                    <span v-if="selectedUnitId === unit.id">&#9660;</span>
+                    <span v-else>&#9654;</span>
+                </div>
+                <div
+                    v-if="
+                        selectedUnitId === unit.id && filteredUsers.length > 0
+                    "
+                    class="mt-2 ml-4"
+                >
+                    <ul>
+                        <li
+                            v-for="user in filteredUsers"
+                            :key="user.id"
+                            class="p-1"
+                        >
+                            {{ user.name }}
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      units: {
-        type: Array,
-        required: true,
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .sidebar {
+</template>
+
+<style scoped>
+.sidebar {
     position: fixed;
     left: 0;
     top: 0;
     background-color: #f7fafc;
-  }
-  </style>
-  
+}
+</style>
