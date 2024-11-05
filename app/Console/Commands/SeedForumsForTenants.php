e9dcab34fc6b38648d6c459f6366a63dfdce81a8
佐藤翔太 <shota19830706@gmail.com>
2024-11-06 05:11:46 +0900

    [update] 全テナントへのフォーラムシーディングコマンドの実装を追加
    この変更により、tenants:seed-forums コマンドを使用して全テナントのデータベースに「Welcome」フォーラムをシーディングする機能が実装されました。テナントごとにシーディングが適用されるよう、テナントコンテキストを初期化してシーダーを実行しています。

--- app/Console/Commands/SeedForumsForTenants.php
+++ app/Console/Commands/SeedForumsForTenants.php
@@ -3,28 +3,32 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Tenant;
+use Database\Seeders\ForumsTableSeeder;
+use Stancl\Tenancy\Facades\Tenancy;
 
 class SeedForumsForTenants extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:seed-forums-for-tenants';
+    protected $signature = 'tenants:seed-forums';
+    protected $description = '全テナントのデータベースにフォーラムデータをシードします';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+        // 全テナントをループして処理
+        Tenancy::query()->each(function ($tenant) {
+            Tenancy::initialize($tenant); // テナントコンテキストを開始
+            
+            // テナントのデータベースでシーダーを実行
+            $this->call(ForumsTableSeeder::class);
+            
+            Tenancy::end(); // テナントコンテキストを終了
+        });
+        
+        return Command::SUCCESS;
     }
 }
