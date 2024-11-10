b63b443f19f378ad1fe602ab3bf00175ab79b090
佐藤翔太 <shota19830706@gmail.com>
2024-11-11 07:11:41 +0900

    [add] いいね機能のためのLikeControllerを作成
    いいね機能のトグル処理を実装する準備として、LikeControllerを作成しました。今後、このコントローラーにいいねの追加・削除処理を記述し、投稿やコメントに対するいいね機能を実現する予定です。

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Like;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function toggleLike(Request $request)
+    {
+        
+    }
+}
+    
