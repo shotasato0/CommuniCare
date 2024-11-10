fac51df1fc9e86169c893114d875df8ec27e8218
佐藤翔太 <shota19830706@gmail.com>
2024-11-11 07:12:56 +0900

    [add] ポリモーフィックリレーションによるいいねのトグル機能をLikeControllerに実装
    LikeControllerに、ポリモーフィックリレーションを利用したいいね機能のトグル処理を実装しました。ユーザーが投稿やコメントにいいねを追加・削除できるようになり、いいね状態といいね数をJSONで返す構成としています。

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -1,15 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
-
 use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
     public function toggleLike(Request $request)
     {
-        
+        $user = Auth::user();
+        $likeableId = $request->input('likeable_id');
+        $likeableType = $request->input('likeable_type');
+
+        // ポリモーフィックリレーションのため、対象モデルを動的に設定
+        $likeableModel = 'App\\Models\\' . $likeableType;
+        $likeable = $likeableModel::findOrFail($likeableId);
+
+        // 既にいいねしているかを確認
+        $existingLike = Like::where('user_id', $user->id)
+            ->where('likeable_id', $likeableId)
+            ->where('likeable_type', $likeableModel)
+            ->first();
+
+        if ($existingLike) {
+            // 既存のいいねがあれば削除
+            $existingLike->delete();
+            $isLiked = false;
+        } else {
+            // 新しいいいねを作成
+            $likeable->likes()->create(['user_id' => $user->id]);
+            $isLiked = true;
+        }
+
+        return response()->json(['isLiked' => $isLiked]);
     }
 }
-    
