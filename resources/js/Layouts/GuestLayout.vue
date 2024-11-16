f7962f5c853d51302706aa236f642df049b71790
佐藤翔太 <shota19830706@gmail.com>
2024-11-16 22:01:40 +0900

    [update] ロゴのレイアウトを中央揃えに変更
    ApplicationLogoコンポーネントを使用して、ロゴを中央揃えのスタイルに修正しました。また、コードの可読性を向上させるため、コメントを追加しました。

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,16 +1,20 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
+    <div
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+    >
+        <!-- ロゴ部分 -->
+        <div class="w-full flex justify-center">
             <Link href="/home">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <ApplicationLogo class="w-full text-gray-500 text-center" />
             </Link>
         </div>
 
+        <!-- コンテンツ部分 -->
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
         >
