c607c945fa80e43f050e05baf0058751dd3353a1
佐藤翔太 <shota19830706@gmail.com>
2024-11-18 08:57:57 +0900

    [update] [update] テナントのドメイン名を動的表示し、ルート定義をリファクタリング
    セッションから取得したテナントのドメイン名を'TenantHome'ビューに渡す処理を追加しました。これにより、ドメイン名を動的に表示できるようになりました。また、TenantHomeControllerを作成し、/homeルートのロジックをコントローラーに移動しました。これにより、ルート定義がシンプルになり、コードの可読性と拡張性が向上しました。

---
+++ app/Http/Controllers/TenantHomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Session;
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
+
+class TenantHomeController extends Controller
+{
+    public function index()
+    {
+        // セッションからドメイン名を取得
+        $domain = Session::get('tenant_domain', '不明なドメイン');
+
+        return Inertia::render('TenantHome', [
+            'canLogin' => \Route::has('login'),
+            'canRegister' => \Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+            'domainName' => $domain,
+        ]);
+    }
+}
+
