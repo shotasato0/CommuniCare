6c57822976ffc48e14370d8efe21fed342b8f50f
佐藤翔太 <shota19830706@gmail.com>
2024-11-18 07:21:35 +0900

    [update] タイトルフォントサイズとロゴの色を調整
    refs #xxx デザインの一貫性を向上させるため、タイトルのフォントサイズを変更し、ロゴの色を青に統一しました。

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <h1
-        class="title-font sm:text-7xl text-3xl mb-4 font-light text-blue-900 tracking-widest"
+        class="title-font sm:text-5xl text-3xl mb-4 font-light text-blue-900 tracking-widest"
     >
         CommuniCare
     </h1>
