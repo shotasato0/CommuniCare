953c660a169c167cc4b303ad93ba20ad9b15c015
佐藤翔太 <shota19830706@gmail.com>
2024-11-23 20:13:22 +0900

    [add]usersテーブルにguest_session_idカラムを追加
    ゲストユーザーの識別とセッション管理を実現するため、guest_session_idカラムを追加しました。

--- database/migrations/tenant/2024_11_23_201144_add_guest_session_id_to_users_table.php
+++ database/migrations/tenant/2024_11_23_201144_add_guest_session_id_to_users_table.php
@@ -4,25 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddGuestSessionIdToUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('guest_session_id')->nullable()->after('password')->comment('セッションIDを格納してゲストを特定する');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('guest_session_id');
         });
     }
-};
+}
+
