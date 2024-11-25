68204e0fc1f753a3ae1a5c2edfda22628131c78e
佐藤翔太 <shota19830706@gmail.com>
2024-11-26 08:54:49 +0900

    [update] ゲストユーザー削除コマンドのロジックを実装
    未使用のゲストユーザーを削除するロジックを CleanupGuestUsers コマンドに実装しました。コマンド名を 'cleanup:guest-users' に変更し、作成から1時間以上経過したゲストユーザーを削除する処理を追加しました。

--- app/Console/Commands/CleanupGuestUsers.php
+++ app/Console/Commands/CleanupGuestUsers.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\User;
 
 class CleanupGuestUsers extends Command
 {
@@ -11,20 +12,24 @@ class CleanupGuestUsers extends Command
      *
      * @var string
      */
-    protected $signature = 'app:cleanup-guest-users';
+    protected $signature = 'cleanup:guest-users';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Delete guest users who have not been active for more than 1 hour';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $deletedCount = User::whereNotNull('guest_session_id')
+            ->where('created_at', '<', now()->subHours(1))
+            ->delete();
+
+        $this->info("Deleted {$deletedCount} guest users.");
     }
 }
