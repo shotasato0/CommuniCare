098d15aea0b8d20c3dcc7e1bf85ee97c396f6d57
佐藤翔太 <shota19830706@gmail.com>
2024-11-26 09:54:52 +0900

    [add] スケジュールタスクでゲストユーザー削除コマンドを登録
    スケジュールタスクに `cleanup:guest-users` コマンドを追加し、1時間ごとに実行されるよう設定しました。

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+use Stancl\Tenancy\Database\Models\Tenant;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+app(Schedule::class)->command('cleanup:guest-users')->hourly();
