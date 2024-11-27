b43f527aee00fb5a54246b9c87d14fe72cd68938
佐藤翔太 <shota19830706@gmail.com>
2024-11-27 23:09:52 +0900

    [update] アプリロゴのカラーをグレーからブルーに変更
    アプリロゴの視認性を向上させるため、カラーを `text-gray-800` から `text-blue-900` に変更しました。これにより、デザイン全体の統一感を向上させました。

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -107,7 +107,7 @@ const confirmGuestLogout = (event) => {
                                     class="cursor-pointer"
                                 >
                                     <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
+                                        class="block h-9 w-auto fill-current text-blue-900"
                                     />
                                 </div>
                             </div>
