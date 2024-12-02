7bbc1201a11fde4d0e2df1e766194577ce0616cf
佐藤翔太 <shota19830706@gmail.com>
2024-12-02 21:38:37 +0900

    [fix] 投稿削除時に引用元更新処理の状態確認を追加
    削除対象の投稿を引用している投稿の更新処理において、更新前後の状態をログで確認するデバッグ処理を追加しました。また、削除後のリダイレクト処理をForumController@indexに統一し、最新の投稿データが正しく反映されるように修正しました。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -44,25 +44,27 @@ public function destroy($id)
         // 削除対象の投稿を取得
         $post = Post::findOrFail($id);
 
-        // 削除対象の投稿を引用している投稿を更新
+        // 削除対象の投稿を引用している投稿を取得
+        $affectedPosts = Post::where('quoted_post_id', $post->id)->get();
+        
+        // デバッグ: 更新前の状態を確認
+        \Log::info('Before update:', ['affectedPosts' => $affectedPosts->toArray()]);
+
+        // 引用投稿のフラグを更新
         Post::where('quoted_post_id', $post->id)
             ->update(['quoted_post_deleted' => true]);
+
+        // 更新後の投稿を取得
+        $updatedPosts = Post::where('quoted_post_id', $post->id)->get();
+
+        // デバッグ: 更新後の状態を確認
+        \Log::info('After update:', ['updatedPosts' => $updatedPosts->toArray()]);
 
         // 削除対象の投稿を削除
         $post->forceDelete();
     });
 
-    // 最新の投稿データを取得
-    $forumId = request('forum_id');
-    $posts = Post::where('forum_id', $forumId)
-        ->with(['user', 'quotedPost', 'comments'])
-        ->latest()
-        ->paginate(5);
-
-    return Inertia::render('Forum', [
-        'posts' => $posts,
-        'selectedForumId' => $forumId,
-    ]);
+    return app(ForumController::class)->index(request());
 }
 
 
