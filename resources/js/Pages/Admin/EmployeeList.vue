506e94ce86a626e07ced995efdc9b9879c3335c5
佐藤翔太 <shota19830706@gmail.com>
2024-12-14 09:37:04 +0900

    [fix] 社員一覧ページのヘッダー表示位置とレイアウトを調整
    社員一覧ページで <template #header> 内の見出し「Employee List」が正しく表示されるよう、ヘッダーとページレイアウトを調整しました。また、余分な余白を削除し、ページ全体の統一感を向上させました。これにより、社員一覧ページが意図したデザイン通りに表示されるようになりました。

--- resources/js/Pages/Admin/EmployeeList.vue
+++ resources/js/Pages/Admin/EmployeeList.vue
@@ -53,6 +53,11 @@ watchEffect(() => {
     <Head :title="$t('Employee List')" />
 
     <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ $t("Employee List") }}
+            </h2>
+        </template>
         <!-- フラッシュメッセージ -->
         <transition name="fade">
             <div
@@ -63,7 +68,7 @@ watchEffect(() => {
             </div>
         </transition>
 
-        <div class="py-12">
+        <div>
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
