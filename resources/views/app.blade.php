283ea052a17e1c75aa75d31af8e48201f7783c1e
佐藤翔太 <shota19830706@gmail.com>
2024-12-19 08:28:45 +0900

    [update] フッターをレイアウトコンポーネントとして再統合
    ef6377cでHTML直書きされたフッターを削除し、Footerコンポーネントを再導入しました。これにより、各ビューにページ下部に固定されたフッターが表示されるようになりました。

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,20 +24,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen flex flex-col">
-        <div class="flex-grow">
-            @inertia
-        </div>
-
-        <footer class="bg-white border-t border-gray-200">
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-                <div class="text-center text-sm text-gray-500">
-                    <!-- フッターの内容 -->
-                    <p>&copy; {{ date('Y') }} CommuniCare. All rights reserved.</p>
-                </div>
-            </div>
-        </footer>
-    </div>
+    @inertia
 </body>
 
 </html>
