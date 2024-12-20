2528c2bc93b401c80b73e2c0db6cecaa8ac07576
佐藤翔太 <shota19830706@gmail.com>
2024-12-20 09:03:09 +0900

    [update]definePropsとdefineEmitsのインポートを削除
    Vue 3.3以降でdefinePropsとdefineEmitsがコンパイラマクロとして自動処理されるため、不要なインポートを削除しました。

--- resources/js/Components/CommentForm.vue
+++ resources/js/Components/CommentForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, defineEmits, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { router } from "@inertiajs/vue3";
 import { getCsrfToken } from "@/Utils/csrf";
 
