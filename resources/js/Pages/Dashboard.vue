9e0ab2857df81df230800bee4b28bc289e12651b
佐藤翔太 <shota19830706@gmail.com>
2025-01-07 19:24:39 +0900

    update] ログイン情報とユーザーアイコンのレイアウトを追加
    ユーザー名とアイコンを右側に表示するレイアウトを追加し、掲示板リンクとログインメッセージを左側に配置しました。また、Inertia.jsのLinkコンポーネントを使用してユーザープロフィールページへの遷移を実装しました。
    これにより、ユーザーが自身のプロフィールにアクセスしやすくなります。

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,7 @@ import { Head, usePage } from "@inertiajs/vue3";
 import { ref, onMounted } from "vue";
 import AdminDashboard from "@/Pages/Admin/Dashboard.vue";
 import { redirectToForum } from "@/Utils/redirectToForum";
+import { Link } from "@inertiajs/vue3";
 
 const { props } = usePage();
 const isAdmin = props.isAdmin;
@@ -46,23 +47,50 @@ console.log("User data:", props.auth.user);
         <div class="pb-12 px-4 sm:px-8 lg:px-16">
             <div class="max-w-6xl mx-auto">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div
-                        class="p-6 text-gray-900 flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4"
-                    >
-                        <div v-if="!isGuest">
-                            {{ $t("You're logged in") }}
-                        </div>
-                        <div v-else>
-                            {{ $t("Logged in as guest user") }}
-                        </div>
-                        <!-- 掲示板へのリンクボタン -->
-                        <button
-                            href="#"
-                            @click.prevent="navigateToForum"
-                            class="text-blue-500 link-hover whitespace-nowrap"
+                    <div class="p-6 text-gray-900">
+                        <div
+                            class="flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0"
                         >
-                            {{ $t("Go to Forum") }}
-                        </button>
+                            <!-- 左側：ログインメッセージと掲示板リンク -->
+                            <div class="flex items-center space-x-4">
+                                <div v-if="!isGuest">
+                                    {{ $t("You're logged in") }}
+                                </div>
+                                <div v-else>
+                                    {{ $t("Logged in as guest user") }}
+                                </div>
+                                <button
+                                    href="#"
+                                    @click.prevent="navigateToForum"
+                                    class="text-blue-500 link-hover whitespace-nowrap"
+                                >
+                                    {{ $t("Go to Forum") }}
+                                </button>
+                            </div>
+
+                            <!-- 右側：ユーザーアイコンと名前 -->
+                            <Link
+                                :href="`/users/${props.auth.user.id}`"
+                                class="flex items-center space-x-4 group hover:bg-gray-50 rounded-lg p-2"
+                            >
+                                <img
+                                    :src="
+                                        props.auth.user.icon
+                                            ? `/storage/${props.auth.user.icon}`
+                                            : '/images/default_user_icon.png'
+                                    "
+                                    alt="Profile Icon"
+                                    class="w-12 h-12 rounded-full"
+                                />
+                                <div class="flex items-center">
+                                    <span
+                                        class="font-bold text-lg text-gray-500 group-hover:text-black transition-colors"
+                                    >
+                                        {{ props.auth.user.name }}
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
