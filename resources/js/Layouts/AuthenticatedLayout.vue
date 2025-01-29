690bed7c352eeaefa428e75ed58db688177cffe8
佐藤翔太 <shota19830706@gmail.com>
2025-01-29 09:05:49 +0900

    [remove] NavLink.vue および ResponsiveNavLink.vue の削除と不要なインポートの整理
    ナビゲーションの実装を div ベースに統一したため、NavLink.vue および ResponsiveNavLink.vue を削除し、関連するインポートを整理しました。

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -4,8 +4,6 @@ import { usePage } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import Dropdown from "@/Components/Dropdown.vue";
 import DropdownLink from "@/Components/DropdownLink.vue";
-import NavLink from "@/Components/NavLink.vue";
-import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { redirectToForum } from "@/Utils/redirectToForum";
 import Footer from "@/Layouts/Footer.vue";
 import CustomDialog from "@/Components/CustomDialog.vue";
