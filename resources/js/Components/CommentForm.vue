82f4dd7623c80d1735a4076ccfcc981347c37287
佐藤翔太 <shota19830706@gmail.com>
2025-02-10 09:40:32 +0900

    [update] コメント入力欄に `required` を追加し、未入力時の即時フィードバックを強化
    これまでコメントが未入力のまま送信しようとすると、コンソールにエラーは表示されるものの、画面上にはエラーが表示されない状態でした。
    今回の変更により、`required` を追加することで、ブラウザの標準機能を活用し、ユーザーに未入力のフィードバックを即座に表示できるようになりました。

--- resources/js/Components/CommentForm.vue
+++ resources/js/Components/CommentForm.vue
@@ -7,23 +7,28 @@ import { handleImageChange } from "@/Utils/imageHandler";
 
 // コメントフォームのプロパティを定義
 const props = defineProps({
-    postId: { // 投稿ID
+    postId: {
+        // 投稿ID
         type: Number,
         required: true,
     },
-    parentId: { // 親コメントID
+    parentId: {
+        // 親コメントID
         type: Number,
         default: null,
     },
-    selectedForumId: { // 選択中の掲示版ID
+    selectedForumId: {
+        // 選択中の掲示版ID
         type: Number,
         required: true,
     },
-    replyToName: { // 返信先のユーザー名
+    replyToName: {
+        // 返信先のユーザー名
         type: String,
         default: "",
     },
-    title: { // フォームのタイトル
+    title: {
+        // フォームのタイトル
         type: String,
         default: "返信",
     },
@@ -33,7 +38,8 @@ const emit = defineEmits(["cancel"]); // キャンセルイベントを発行す
 const placeholder = ref(`@${props.replyToName} さんへの返信を入力`); // プレースホルダー
 
 // コメントデータを管理するref
-const commentData = ref({ // コメントデータ
+const commentData = ref({
+    // コメントデータ
     post_id: props.postId, // 投稿ID
     parent_id: props.parentId, // 親コメントID
     message: "", // コメントメッセージ
@@ -91,14 +97,16 @@ const submitComment = () => {
     }
 
     // 画像データが存在する場合はフォームデータに追加
-    if (img.value) { // 画像データが存在する場合
+    if (img.value) {
+        // 画像データが存在する場合
         formData.append("img", img.value); // 画像データをフォームデータに追加
     }
 
     // コメントの投稿処理を実行
     router.post(route("comment.store", { post: props.postId }), formData, {
         preserveScroll: true, // スクロール位置を維持
-        onSuccess: () => { // 投稿成功時の処理
+        onSuccess: () => {
+            // 投稿成功時の処理
             commentData.value = {
                 post_id: props.postId, // 投稿IDをリセット
                 parent_id: props.parentId, // 親コメントIDをリセット
@@ -114,7 +122,8 @@ const submitComment = () => {
                 }
             );
         },
-        onError: (errors) => { // 投稿失敗時の処理
+        onError: (errors) => {
+            // 投稿失敗時の処理
             console.error("コメントの投稿に失敗しました:", errors); // エラーメッセージをコンソールに出力
         },
     });
@@ -136,14 +145,19 @@ const handleCancel = () => {
 
 <template>
     <div class="mt-4">
+        <!-- コメントフォーム -->
         <form @submit.prevent="submitComment" enctype="multipart/form-data">
             <div class="relative">
+
+                <!-- コメントメッセージ入力欄 -->
                 <textarea
                     v-model="commentData.message"
                     class="w-full p-2 border rounded-md"
+                    required
                     :placeholder="placeholder"
                     rows="3"
                 ></textarea>
+
                 <!-- ファイル選択アイコン -->
                 <div
                     class="absolute right-2 bottom-4 bg-gray-300 text-black transition hover:bg-gray-400 hover:text-white rounded-md flex items-center justify-center cursor-pointer"
@@ -154,6 +168,8 @@ const handleCancel = () => {
                     <i class="bi bi-card-image text-2xl"></i>
                 </div>
             </div>
+
+            <!-- 隠しファイル入力 -->
             <input
                 type="file"
                 accept="image/*"
@@ -167,12 +183,14 @@ const handleCancel = () => {
             </div>
             <!-- プレビュー表示 -->
             <div v-if="imgPreview" class="relative mt-2 inline-block">
+                <!-- プレビュー画像 -->
                 <img
                     :src="imgPreview"
                     alt="画像プレビュー"
                     class="w-32 h-32 object-cover rounded-md cursor-pointer hover:opacity-80 transition"
                     @click="isModalOpen = true"
                 />
+                <!-- プレビュー画像削除ボタン -->
                 <div
                     class="absolute top-0 right-0 bg-white rounded-full p-1 cursor-pointer flex items-center justify-center"
                     @click="removeImage"
@@ -184,13 +202,18 @@ const handleCancel = () => {
                     ></i>
                 </div>
             </div>
+
+            <!-- コメント画像モーダル -->
             <ImageModal :isOpen="isModalOpen" @close="isModalOpen = false">
                 <img
                     :src="imgPreview"
                     class="max-w-full max-h-full rounded-lg"
                 />
             </ImageModal>
+
+            <!-- コメントフォームボタン群 -->
             <div class="flex justify-end space-x-2 mt-2">
+                <!-- キャンセルボタン -->
                 <button
                     type="button"
                     @click="handleCancel"
@@ -198,6 +221,8 @@ const handleCancel = () => {
                 >
                     <i class="bi bi-x-lg"></i>
                 </button>
+
+                <!-- コメント送信ボタン -->
                 <button
                     type="submit"
                     class="my-2 px-4 py-2 rounded-md bg-blue-100 text-blue-700 transition hover:bg-blue-300 hover:text-white cursor-pointer"
