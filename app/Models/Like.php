7a806671609ee0d3677af7145191692a4ae990fe
佐藤翔太 <shota19830706@gmail.com>
2025-03-07 09:43:56 +0900

    [update] いいねモデルにポリモーフィックリレーションとユーザー取得機能を追加
    いいねしたユーザー一覧を取得し、ツールチップで表示するために、ポリモーフィックリレーションとユーザー情報の取得メソッドを追加しました。

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -9,11 +9,18 @@ class Like extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['tenant_id', 'user_id'];
+    protected $fillable = ['tenant_id', 'user_id', 'likeable_id', 'likeable_type'];
 
+    // いいねしたモデルを取得
     public function likeable()
     {
         return $this->morphTo(); // ポリモーフィックリレーション
+    }
+
+    // いいねしたユーザーを取得
+    public function user()
+    {
+        return $this->belongsTo(User::class); // 多対一のリレーション
     }
 }
 
