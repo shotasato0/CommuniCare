48f47c3c612f3072bcf4a2a3d05861d64068468a
佐藤翔太 <shota19830706@gmail.com>
2025-08-04 17:27:13 +0900

    [update] アプリタイトルにダークモード用の文字色を適用
    暗い背景でもタイトルが視認できるよう、`dark:text-blue-300`を追加してダークモードに対応しました。

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <h1
-        class="title-font sm:text-3xl text-2xl mb-2 font-light text-blue-900 tracking-widest"
+        class="title-font sm:text-3xl text-2xl mb-2 font-light text-blue-900 dark:text-blue-300 tracking-widest"
     >
         CommuniCare
     </h1>
