458ba3d2c71f6622fb512298ae422ab870c690dd
佐藤翔太 <shota19830706@gmail.com>
2025-08-08 15:30:03 +0900

    [add]管理者登録リクエストバリデーションクラスの追加
    管理者登録機能のバリデーション処理を統一するため、AdminRegisterRequestクラスを新規作成しました。名前・ユーザー名の必須チェック・文字数制限・テナント内重複チェック、パスワードの最小文字数・確認入力一致チェックのバリデーションルールと日本語エラーメッセージを定義しています。

---
+++ app/Http/Requests/Admin/AdminRegisterRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * 管理者登録リクエスト
+ */
+class AdminRegisterRequest extends FormRequest
+{
+    /**
+     * 認可
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションルール
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'username_id' => 'required|string|max:255|unique:users,username_id,NULL,id,tenant_id,' . tenant('id'),
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+
+    /**
+     * エラーメッセージ
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必須です。',
+            'name.string' => '名前は文字列で入力してください。',
+            'name.max' => '名前は255文字以内で入力してください。',
+            'username_id.required' => 'ユーザー名は必須です。',
+            'username_id.string' => 'ユーザー名は文字列で入力してください。',
+            'username_id.max' => 'ユーザー名は255文字以内で入力してください。',
+            'username_id.unique' => 'このユーザー名は既に使用されています。',
+            'password.required' => 'パスワードは必須です。',
+            'password.string' => 'パスワードは文字列で入力してください。',
+            'password.min' => 'パスワードは8文字以上で入力してください。',
+            'password.confirmed' => 'パスワード確認が一致していません。',
+        ];
+    }
+}
