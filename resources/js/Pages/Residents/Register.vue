810813ff5dcf99e7d6d44847682ad2b61413f3b5
佐藤翔太 <shota19830706@gmail.com>
2025-08-08 14:47:54 +0900

    [update] 入居者登録画面の見出しにダークモード対応のテキスト色を追加
    ダークモード時に見出しの文字色が背景と同化して視認性が低下していたため、`dark:text-gray-200` を追加しました。

--- resources/js/Pages/Residents/Register.vue
+++ resources/js/Pages/Residents/Register.vue
@@ -20,7 +20,7 @@ const submit = () => {
 <template>
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ $t("Resident Registration") }}
             </h2>
         </template>
