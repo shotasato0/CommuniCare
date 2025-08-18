3b3b164666953c6235a483fd9a470ef232b4c33b
佐藤翔太 <shota19830706@gmail.com>
2025-08-18 15:42:01 +0900

    [add] MigrateToAttachmentSystem Artisanコマンドの基盤クラスを作成
    Phase 2-1データ移行コマンド実装において、users.icon・posts.img・comments.imgから新Attachmentシステムへの包括的移行を実行するArtisanコマンドの基盤を作成。今後の実装で3モデル統一移行ロジック・バッチ処理・検証機能を追加予定。

---
+++ app/Console/Commands/MigrateToAttachmentSystem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MigrateToAttachmentSystem extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:migrate-to-attachment-system';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
