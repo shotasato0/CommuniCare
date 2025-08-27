880cae9fd1a31510b906b80d4c30ba9c3fb4a4a4
佐藤翔太 <shota19830706@gmail.com>
2025-08-28 07:58:34 +0900

    [fix] Users/Indexページのユーザーアイコン表示処理を統一仕様に修正
    ユーザー一覧ページでアイコンが正常表示されない問題を解決するため、パス生成ロジックを他のコンポーネントと統一し（/storage/プレフィックスの適切な処理）、画像読み込み失敗時のデフォルトアイコン表示機能を追加しました。これによりユーザー管理機能でも堅牢なアイコン表示処理が適用されます。

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -385,12 +385,15 @@ const totalFilteredUsers = computed(() => {
                                         >
                                             <img
                                                 :src="
-                                                    user.icon
-                                                        ? `/storage/${user.icon}`
+                                                    user.icon 
+                                                        ? (user.icon.startsWith('/storage/') 
+                                                            ? user.icon 
+                                                            : `/storage/${user.icon}`)
                                                         : '/images/default_user_icon.png'
                                                 "
                                                 alt="Profile Icon"
                                                 class="w-12 h-12 rounded-full"
+                                                @error="$event.target.src='/images/default_user_icon.png'"
                                             />
                                             <div
                                                 class="flex justify-between items-start w-full"
