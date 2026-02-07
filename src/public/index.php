6c23a4cb5392ee10c22af2e9ceb3c6db29843d1d
佐藤翔太 <83856475+shotasato0@users.noreply.github.com>
2026-02-07 15:49:57 +0900

    プロジェクトルールの見直し|artisanとpublic/index.phpのパスをsrc/構造に合わせて修正 #198

--- src/public/index.php
+++ src/public/index.php
@@ -4,13 +4,37 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any of our classes manually.
+|
+*/
+
+$basePath = dirname(__DIR__);
+require $basePath.'/vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight these users.
+|
+*/
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
